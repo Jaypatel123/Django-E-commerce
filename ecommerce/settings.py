95b59b097d8d6b764f4c663a4906f792a01dedf5
Jaypatel123 <jaypatel97043@gmail.com>
2023-12-16 14:55:40 -0500

    Implemented built-in caching to load resource-intensive web pages more quickly

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -231,6 +231,12 @@ DATABASES = {
 
 }
 '''
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+#         'LOCATION': 'unique-snowflake',
+#     }
+# }
 
 
 
